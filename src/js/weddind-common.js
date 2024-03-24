7e968a044a2c5f188d745a267a56d2b06c9a3d6b
Sharky <loginov@2cherryline.ru>
2024-03-24 22:54:21 +0300

    WIP

--- src/js/weddind-common.js
+++ src/js/weddind-common.js
@@ -1,6 +1,13 @@
 import {Fancybox} from "@fancyapps/ui";
 
+window.addEventListener('load', () => {
+    const preloader = document.querySelector('#preloader');
+    
+    if (preloader) {
+        preloader.style.display = 'none';
+    }
+});
+
 Fancybox.bind("[data-fancybox]", {
     height: '100%',
 });
-
